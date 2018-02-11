998b79df5d014ba60638379ae4afbbf6efed3e2d
Ray Nathan Low <raynathanlow@gmail.com>
2018-02-10 20:58:18 -0800

    Fixed main and update

--- assets/js/update.js
+++ assets/js/update.js
@@ -1,5 +1,5 @@
 function updateGameArea() {
-    // console.log(state);
+    console.log(state);
     // console.log(isChinUp);
     // console.log(currEnergy);
     if (state == END) {
@@ -20,7 +20,6 @@ function updateGameArea() {
         // state = MAIN_MENU;
     }
 
-<<<<<<< HEAD
     if (state == PLAY) {
         myGameArea.clear();
         player.angle = 0;
@@ -32,86 +31,31 @@ function updateGameArea() {
 
         if(chinInterval > BUTTON_INTERVAL && (myGameArea.keys && myGameArea.keys[38])) {
 
-            if(currStrength <= -100 || isChinUp) {
-                if (isChinUp) { // going down
-                    // player.y += chinUpDist;
-                    document.getElementById("ted").src= "assets/Teddy/Ted_0.png"; // change ted image
 
-                    isChinUp = false;
-                    currStrength = 0;
-                } else { // going up
-                    // player.y -= chinUpDist;
-                    document.getElementById("ted").src= "assets/Teddy/Ted_1.png";
-                    currStrength = 0;
-                    counter++;
-                    isChinUp = true;
-                    currEnergy += ENERGY_LOSS;
-                    var grunt = new Audio('grunt.mp3').play();
-                }
-                chinInterval = 0;
+            if (isChinUp) { // going down
+                // player.y += chinUpDist;
+                document.getElementById("ted").src= "assets/Teddy/Ted_0.png"; // change ted image
+
+                isChinUp = false;
+
+            } else { // going up
+                // player.y -= chinUpDist;
+                document.getElementById("ted").src= "assets/Teddy/Ted_1.png";
+
+                isChinUp = true;
+                currEnergy += ENERGY_LOSS;
+                var grunt = new Audio('grunt.mp3').play();
             }
+            chinInterval = 0;
+
         }
 
-        //pressing spacebar rejuvinates strength
-        if(myGameArea.keys && myGameArea.keys[32]) {
-            if (currStrength <= -100) {
-                currStrength = -100;
-            }
-            if (!isChinUp) {
-                currStrength -= STRENGTH_GAIN;
-            }
-        }
-=======
-if (state == PLAY) {
-myGameArea.clear();
-player.angle = 0;
-player.speed = 0;
-if (food.x >= 480) {
-    food.foodNum = getRandomInt(4);
-    food.x = 0;
-}
-    
-if(chinInterval > BUTTON_INTERVAL && (myGameArea.keys && myGameArea.keys[38])) {
-    
-    
-        if (isChinUp) { // going down
-            // player.y += chinUpDist;
-            document.getElementById("ted").src= "assets/Teddy/Ted_0.png"; // change ted image
-
-            isChinUp = false;
-            
-		} else { // going up
-            // player.y -= chinUpDist;
-            document.getElementById("ted").src= "assets/Teddy/Ted_1.png";
-            
-            isChinUp = true;
-            currEnergy += ENERGY_LOSS;
-            var grunt = new Audio('grunt.mp3').play();
-        }
-        chinInterval = 0;
-    
-}
-
-
-if (alive) {
-    currEnergy += constEnergyLoss;
-	if (isChinUp) {
-		currEnergy += ENERGY_LOSS;
-	}
-}
->>>>>>> 738d375c128e1011a583024020461e1a83080b2f
-
-        if (state == END) {
-            console.log("END");
-            $(".menu").show();
-            $('.replay').show();
-            myGameArea.stop();
-
-<<<<<<< HEAD
-        }
 
         if (alive) {
             currEnergy += constEnergyLoss;
+            if (isChinUp) {
+                currEnergy += ENERGY_LOSS;
+            }
         }
 
         if (currEnergy > 0) {
@@ -120,7 +64,15 @@ if (alive) {
             // myGameArea.stop();
         }
 
-        if (isChinUp && (food.x > 210 && food.x < 255)) {
+        if (state == END) {
+            console.log("END");
+            $(".menu").show();
+            $('.replay').show();
+            // myGameArea.stop();
+        }
+
+        if (isChinUp && (food.y < 18 && food.y > 13 && food.x > 210 && food.x < 255 )){
+
             if (currEnergy - food.energyGain >= MAX_ENERGY) {
                 currEnergy -= food.energyGain;
             } else {
@@ -134,26 +86,6 @@ if (alive) {
         food.newPos();
         food.update();
         healthBar.update();
-        strength.update();
-        pointCounter.update();
         chinInterval++;
     }
-=======
-if (isChinUp && (food.y < 18 && food.y > 13 && food.x > 210 && food.x < 255 )){
-	
-    if (currEnergy - food.energyGain >= MAX_ENERGY) {
-        currEnergy -= food.energyGain;
-    } else {
-        currEnergy = MAX_ENERGY;
-    }
-    food.foodNum = getRandomInt(4);
-    food.x = 0;
-    console.log("score");
-}
-player.update();
-food.newPos();
-food.update();
-healthBar.update();
-chinInterval++;
->>>>>>> 738d375c128e1011a583024020461e1a83080b2f
 }
